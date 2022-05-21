3e8b6f4552dcbc5ef83e719f3a2d40646ddc1504
shinchan1404 <86519253+shinchan1404@users.noreply.github.com>
2022-05-21 14:46:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song=""
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center()
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses );
+}
+function modelLoaded(){
+    console.log("posenet iS initialize")
+}
+function draw(){
+    image(video,0,0,400,400);
+    fill("red")
+      circle(leftWristX,leftWristY,15);
+    circle(rightWristX,rightWristY,15);
+}
+function preload(){
+    song=loadSound("music.mp3");
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function stop(){
+    song.stop();
+}
+function gotPoses(results){
+    console.log("welcome");
+    if(results.length>0){
+        console.log(results)
+        leftWristX=results[0].pose.leftWrist.x
+        leftWristY=results[0].pose.leftWrist.y
+        console.log(leftWristX, leftWristY)
+        rightWristX=results[0].pose.rightWrist.x
+        rightWristY=results[0].pose.rightWrist.y
+        console.log(rightWristX, rightWristY)
+      
+    }
+}
